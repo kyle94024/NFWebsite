3719b8d7b069b1c9bc33beb5a3902b78a65419de
Moazzam Ali <moazzam435j@gmail.com>
2024-10-25 15:02:59 +0500

    large screens layout improvements

--- src/components/ArticleCardSkeleton/ArticleCardSkeleton.jsx
+++ src/components/ArticleCardSkeleton/ArticleCardSkeleton.jsx
@@ -2,12 +2,20 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export function ArticleCardSkeleton() {
     return (
-        <div className="flex flex-col w-full space-y-3">
+        <div className="flex flex-col w-full gap-8">
             <Skeleton className="h-[250px] w-full rounded-xl" />
             <div className="space-y-4">
+                <Skeleton className="h-6 w-[100px]" />
                 <Skeleton className="h-8 w-full" />
                 <Skeleton className="h-8 w-full" />
-                <Skeleton className="h-4 w-[200px]" />
+                <Skeleton className="h-8 w-[90%]" />
+            </div>
+            <div className="flex items-center space-x-4">
+                <Skeleton className="h-20 w-20 rounded-full" />
+                <div className="space-y-4">
+                    <Skeleton className="h-6 w-[250px]" />
+                    <Skeleton className="h-6 w-[200px]" />
+                </div>
             </div>
         </div>
     );
